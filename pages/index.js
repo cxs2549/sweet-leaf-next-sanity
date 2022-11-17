06804528f7f06a244b213036620580129582c72d
Cliff Sanchez <sanchez.cliff.86@gmail.com>
2022-11-16 18:52:32 -0800

    add title to head

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import React from "react"
+import Head from "next/head"
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+    <>
+      <Head>
+        <title>Sweet Leaf</title>
+      </Head>
+    </>
   )
 }
 
