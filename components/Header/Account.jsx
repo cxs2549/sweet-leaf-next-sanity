701e75ed5bbab576608e52eb7952f97eaf35e979
Cliff Sanchez <sanchez.cliff.86@gmail.com>
2022-11-17 14:58:56 -0800

    add menu/account slide-ins

--- components/Header/Account.jsx
+++ components/Header/Account.jsx
@@ -1,13 +1,72 @@
-import React from "react"
+import React, { useState, useRef, useEffect } from "react"
 import { FiUser } from "react-icons/fi"
+import { motion, AnimatePresence } from "framer-motion"
+import { MdClose } from "react-icons/md"
+
 const Account = () => {
+  const [open, setOpen] = useState(false)
+  const ref = useRef()
+  useOnClickOutside(ref, () => setOpen(false))
   return (
-    <div>
-      <button aria-label="Account" className="btn grid place-items-center">
+    <div ref={ref}>
+      <button
+        onClick={() => setOpen(true)}
+        aria-label="Account"
+        className="btn grid place-items-center"
+      >
         <FiUser size={26} />
       </button>
+      <AnimatePresence>
+        {open && (
+          <div
+            onClick={() => setOpen(false)}
+            className="fixed inset-0 bg-black/25 z-10"
+          ></div>
+        )}
+      </AnimatePresence>
+      <AnimatePresence>
+        {open && (
+          <motion.div
+            initial={{ x: "100%" }}
+            animate={{ x: 0 }}
+            exit={{ x: "100%" }}
+            transition={{ type: "tween" }}
+            className="fixed w-full max-w-[375px] h-full overflow-y-auto right-0  z-10 top-0 bg-white"
+          >
+            <div className="h-[66px] flex items-center justify-between px-4 border-b">
+              <h1 className="text-2xl font-bold">Account</h1>
+              <button
+                onClick={() => setOpen(false)}
+                className="btn grid place-items-center"
+              >
+                <MdClose size={26} />
+              </button>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   )
+}
+
+function useOnClickOutside(ref, handler) {
+  useEffect(() => {
+    const listener = (event) => {
+      if (!ref.current || ref.current.contains(event.target)) {
+        return
+      }
+
+      handler(event)
+    }
+
+    document.addEventListener("mousedown", listener)
+    document.addEventListener("touchstart", listener)
+
+    return () => {
+      document.removeEventListener("mousedown", listener)
+      document.removeEventListener("touchstart", listener)
+    }
+  }, [ref, handler])
 }
 
 export default Account
--- components/Header/Menu.jsx
+++ components/Header/Menu.jsx
@@ -2,6 +2,7 @@ import React, { useState, useRef, useEffect } from "react"
 import { HiMenuAlt4 } from "react-icons/hi"
 import { motion, AnimatePresence } from "framer-motion"
 import { MdClose } from "react-icons/md"
+import { BsChevronRight } from "react-icons/bs"
 const Menu = () => {
   const [open, setOpen] = useState(false)
   const ref = useRef()
@@ -17,21 +18,81 @@ const Menu = () => {
       </button>
       <AnimatePresence>
         {open && (
+          <div
+            onClick={() => setOpen(false)}
+            className="fixed z-10 inset-0 bg-black/25"
+          ></div>
+        )}
+      </AnimatePresence>
+      <AnimatePresence>
+        {open && (
           <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            className="fixed inset-x-0 p-2 z-10 top-0"
+            initial={{ x: "-100%" }}
+            animate={{ x: 0 }}
+            exit={{ x: "-100%" }}
+            transition={{ type: "tween" }}
+            className="fixed max-w-[375px] h-full overflow-y-auto left-0  z-10 top-0"
           >
-            <div className="bg-white shadow h-[500px] p-4 rounded-md">
-              <div className="h-[44px] flex items-center justify-between">
-                <h1>Menu</h1>
+            <div className="bg-white shadow max-w-[375px] overflow-y-auto h-full">
+              <div className="h-[66px] flex items-center justify-between px-4 border-b">
+                <h1 className="text-2xl font-bold">Menu</h1>
                 <button
                   onClick={() => setOpen(false)}
                   className="btn grid place-items-center"
                 >
                   <MdClose size={26} />
                 </button>
+              </div>
+
+              {/* categories */}
+              <div className="border-b pb-4">
+                <div className="h-[66px] flex items-center justify-between px-4">
+                  <h2 className="text-xl font-bold">Categories</h2>
+                  <button
+                    onClick={() => setOpen(false)}
+                    className="btn grid place-items-center"
+                  >
+                    <BsChevronRight size={26} />
+                  </button>
+                </div>
+                <ul className="px-4 pt-2 grid grid-cols-4 gap-4">
+                  {[1, 2, 3, 4, 5, 6, 7, 8].map((item) => (
+                    <li
+                      key={item}
+                      className="flex flex-col items-center justify-center gap-2"
+                    >
+                      <div className="rounded-full bg-neutral-100 h-20 w-20"></div>
+                      <p className="text-xs">Title</p>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              {/* deals */}
+              <div className="border-b pb-4">
+                <div className="h-[66px] flex items-center justify-between px-4">
+                  <h2 className="text-xl font-bold">Deals</h2>
+                </div>
+                <ul className="px-4 pt-2 grid grid-cols-4 gap-y-4">
+                  {[1, 2, 3, 4].map((item) => (
+                    <li
+                      key={item}
+                      className="flex flex-col items-center justify-center gap-2"
+                    >
+                      <div className="rounded-full bg-neutral-100 h-20 w-20"></div>
+                      <p className="text-xs">Title</p>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              {/* pickup */}
+              <div className="border-b pb-4">
+                <div className="h-[66px] flex items-center justify-between px-4">
+                  <h2 className="text-xl font-bold">Pickup & Delivery</h2>
+                </div>
+                <div className="px-4 grid grid-cols-2 gap-4">
+                  <div className="bg-neutral-100 rounded-md p-4">pickup</div>
+                  <div className="bg-neutral-100 rounded-md p-4">delivery</div>
+                </div>
               </div>
             </div>
           </motion.div>
