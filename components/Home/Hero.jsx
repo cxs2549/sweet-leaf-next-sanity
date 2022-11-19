89e9fbb8617a6fffd1481e4c1b0a4c901156fbb7
Cliff Sanchez <sanchez.cliff.86@gmail.com>
2022-11-19 11:52:21 -0800

    tweak hero for performance

--- components/Home/Hero.jsx
+++ components/Home/Hero.jsx
@@ -3,12 +3,12 @@ import React from "react"
 const Hero = () => {
   return (
     <div
-      style={{ backgroundImage: "url(/hero.jpg)" }}
-      className="h-[45vh] md:h-[30vh] bg-bottom bg-cover flex flex-col md:flex-row md:gap-12 items-center justify-center"
+      // style={{ backgroundImage: "url(/hero.jpg)" }}
+      className="py-12 bg-bottom bg-cover flex flex-col md:flex-row md:gap-12 items-center justify-center"
     >
-      <div className="flex flex-col items-center justify-center shadows">
+      <div className="flex flex-col items-center justify-center">
         <h1 className="text-8xl font-bold text-green-500 tracking-tighter">Green</h1>
-        <h1 className="text-8xl font-bold text-white -translate-y-4 tracking-tighter">Friday</h1>
+        <h1 className="text-8xl font-bold text-black -translate-y-4 tracking-tighter">Friday</h1>
       </div>
       <div className="flex flex-col items-center justify-center">
         <p className="text-center max-w-xs text-white text-2xl font-bold">
